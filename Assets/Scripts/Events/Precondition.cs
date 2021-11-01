a959fe7f910172512bda80d4b4d6787e07c87d10
dGameBoy101b <rhysmader@gmail.com>
2021-11-01 23:15:01 +0800

    Revert "Added Event Preconditions"
    This reverts commit f41c0783a19781e0dc3223427475098221769513.

--- Assets/Scripts/Events/Precondition.cs
+++
@@ -1,9 +0,0 @@
-/** The abstract base class for event preconditions */
-public abstract class Precondition
-{
-	/** Test if the given student fulfills this precondition
-	 * \param cha The student to test
-	 * \return True if this precondition is fulfilled by the given student
-	 */
-	public abstract bool Fulfilled(Student cha);
-}
