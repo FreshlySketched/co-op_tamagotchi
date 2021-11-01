292382b299651e189f6f872d6fb9679b0808dd37
dGameBoy101b <rhysmader@gmail.com>
2021-11-02 01:58:46 +0800

    Improved Character Selection
    Created GUI Frame Template prefab.
    Reordered Character data.
    Added confirmation functionality to CharacterSelector.
    Created Character Selection prefab.
    Created Character Confirm Pane prefab.
    Changed CharacterSelectRandomiser to use existing children rather than spawn new children.

---
+++ Assets/Scripts/Character Select/CharacterSelector.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/** A class which can create a student from a given character if invoked
+ * \author Rhys Mader
+ * \date 11 Sep 2021
+ */
+public class CharacterSelector : MonoBehaviour
+{
+	[SerializeField()]
+	[Tooltip("The character to use for a new student if selected")]
+	public Character TargetCharacter;
+
+	[Header("Character Select Elements")]
+
+	[SerializeField()]
+	[Tooltip("The game object to have enabled while selecting character")]
+	private GameObject _selectPane;
+
+	[SerializeField()]
+	[Tooltip("The text element to place the character name in")]
+	private Text _selectText;
+
+	[SerializeField()]
+	[Tooltip("The image element to place the character sprite in")]
+	private Image _selectSprite;
+	
+	[Header("Confirmation Elements")]
+	
+	[SerializeField()]
+	[Tooltip("The game object to have enabled while confirming character selection")]
+	private GameObject _confirmPane;
+	
+	[SerializeField()]
+	[Tooltip("The text element to place the character description in")]
+	private Text _confirmText;
+	
+	[SerializeField()]
+	[Tooltip("The image element to place the character sprite in")]
+	private Image _confirmSprite;
+	
+	/** Create a student from the target character and set it as the active student */
+	public void CreateStudent()
+	{
+		Student.ActiveStudent = new Student(this.TargetCharacter);
+		Debug.Log("Active Student = " + Student.ActiveStudent.Character.DisplayName);
+	}
+
+	/** Setup the selection button for this character selector
+	 * \note Should be done before showing the selection button if the target character has changed
+	 */
+	public void SetupSelection()
+	{
+		this._selectText.text = this.TargetCharacter.DisplayName;
+		this._selectSprite.sprite = this.TargetCharacter.SelectSprite;
+	}
+
+	/** Show the selection pane */
+	public void ShowSelection()
+	{
+		this._selectPane.SetActive(true);
+	}
+
+	/** Hide the selection pane */
+	public void HideSelection()
+	{
+		this._selectPane.SetActive(false);
+	}
+	
+	/** Setup the confirmation pane for this character selector 
+	 * \note Should be done before showing the confirmation pane if the target character has changed
+	 */
+	public void SetupConfirmation()
+	{
+		this._confirmText.text = this.TargetCharacter.SelectDescription;
+		this._confirmSprite.sprite = this.TargetCharacter.SelectSprite;
+	}
+	
+	/** Show the confirmation pane */
+	public void ShowConfirmation()
+	{
+		this._confirmPane.SetActive(true);
+	}
+	
+	/** Hide the confirmation pane */
+	public void HideConfirmation()
+	{
+		this._confirmPane.SetActive(false);
+	}
+}
